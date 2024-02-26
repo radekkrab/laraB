35eb878495f0154c93c0899e813f34a9e6203893
radekkrab <radxxx7@mail.ru>
2024-02-26 12:49:59 +0500

    inerface index and show

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -2,12 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Worker;
 use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
     public function index()
     {
-        return 'this is controller of wc';
+        $workers = Worker::all();
+
+        return view('worker.index', compact('workers'));
+    }
+
+    public function show(Worker $worker)
+    {
+        return view('worker.show', compact('worker'));
+    }
+
+    public function create()
+    {
+        $worker = [
+            'name' => 'Mark',
+            'surname' => 'MarIvanov',
+            'email' => 'miv@mail.ru',
+            'age' => '22',
+            'description' => 'i am Mark',
+            'is_married' => false,
+
+        ];
+
+        Worker::create($worker);
+
+        return 'Ivan was created';
+    }
+
+    public function update()
+    {
+        $workers = Worker::find(2);
+
+        $workers->update([
+            'name' => 'Karl',
+            'surname' => 'Sidorov',
+        ]);
+
+        return 'Was updated';
+    }
+
+    public function delete()
+    {
+        $workers = Worker::find(2);
+
+        $workers->delete();
+
+        return 'Was deleted';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/workers', [\App\Http\Controllers\WorkerController::class, 'index']);
+Route::get('/workers', [\App\Http\Controllers\WorkerController::class, 'index'])->name('worker.index');
+
+Route::get('/workers/{worker}', [\App\Http\Controllers\WorkerController::class, 'show'])->name('worker.show');
+
+Route::get('/workers/create', [\App\Http\Controllers\WorkerController::class, 'create'])->name('worker.create');
+
+Route::get('/workers/update', [\App\Http\Controllers\WorkerController::class, 'update'])->name('worker.update');
+
+Route::get('/workers/delete', [\App\Http\Controllers\WorkerController::class, 'delete'])->name('worker.delete');
